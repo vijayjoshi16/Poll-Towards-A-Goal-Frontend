1044e7d714f02627735054e1eaff11018c8da2dd
vijayjoshi16 <joshivijay324@gmail.com>
2021-02-12 12:10:21 +0530

    Minor changes

--- src/components/Screens/SignIn/SignIn.js
+++ src/components/Screens/SignIn/SignIn.js
@@ -69,7 +69,6 @@ const SignIn = ()=>{
                         });
                     localStorage.setItem("user",result.token);
                     history.push('/allorganizationpolls');
-                    window.location.reload();
                 }else{
                     toast.error('Invalid credentials!', {
                         position: "top-right",
